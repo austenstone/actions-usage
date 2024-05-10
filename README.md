0a85ffb3fad3a149b608d8de35faecb911da326e
Austen Stone <austenstone@github.com>
2024-05-10 14:42:56 -0400

    chore: Update usage-org.yml workflow to include minutes and total paid minutes used

--- README.md
+++ README.md
@@ -21,6 +21,12 @@ This endpoint works with the following token types:
 
 The token must have the following permission set:
 
+If getting the usage for an organization:
+
+* organization_administration:read
+
+If getting the usage for a repository:
+
 * `actions:read`
 
 This endpoint can be used without authentication or the aforementioned permissions if only public resources are requested.
@@ -38,6 +44,8 @@ jobs:
     steps:
       - uses: austenstone/actions-usage@v1
         id: usage
+        with:
+          org: ${{ github.repository_owner }}
       - run: echo "Total usage: ${{ steps.usage.outputs.total }}"
       - if: ${{ steps.usage.outputs.total > 50000 }}
         run: echo "Actions Usage is over 50,000 minutes!"
@@ -52,6 +60,7 @@ Use this action in combination with other actions to notify users.
 - [jira-create-issue](https://github.com/marketplace/actions/jira-create-issue)
 
 #### Example Notification Workflow
+
 ```yml
 name: Actions Usage
 on:
@@ -79,6 +88,31 @@ jobs:
           SLACK_BOT_TOKEN: ${{ secrets.SLACK_BOT_TOKEN }}
 ```
 
+#### Example Switch Runner Workflow
+
+Use this action to switch to a self-hosted runner when you've reached a usage limit.
+
+```yml
+name: Actions Usage
+on:
+  schedule:
+    - cron: '0 0 * * *'
+
+jobs:
+  get-usage:
+    runs-on: ubuntu-latest
+    outputs:
+      total: ${{ steps.usage.outputs.total }}
+    steps:
+      - uses: austenstone/actions-usage@v1
+        id: usage
+  run:
+    runs-on: ${{ steps.get-usage.outputs.total > 50000 ? 'my-self-hosted-runner' : 'ubuntu-latest' }}
+    needs: get-usage
+    steps:
+      - run: echo "Running"
+```
+
 ## ➡️ Inputs
 Various inputs are defined in [`action.yml`](action.yml):
 
@@ -94,7 +128,8 @@ Various inputs are defined in [`action.yml`](action.yml):
 | macos | The usage in minutes for macOS |
 | windows | The usage in minutes for Windows |
 | total | The total usage in minutes |
-
+| included_minutes | The included minutes |
+| total_paid_minutes_used | The total paid minutes used |
 
 ## Further help
 To get more help on the Actions see [documentation](https://docs.github.com/en/actions).
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ interface Input {
   token: string;
   workflows: string;
   org: string;
-  repo: string;
+  repo: string | null;
 }
 
 const getInputs = (): Input => {
@@ -13,9 +13,11 @@ const getInputs = (): Input => {
   result.token = getInput("github-token");
   result.org = getInput("org");
   result.repo = getInput("repo");
-  if (result.repo.includes("/")) {
+  if (!result.org && result.repo.includes("/")) {
     const parts = result.repo.split("/");
     result.repo = parts[1];
+  } else {
+    result.repo = null;
   }
   result.workflows = getInput("workflows");
   if (!result.token || result.token === "") {
@@ -32,11 +34,6 @@ export const run = async (): Promise<void> => {
   const input = getInputs();
   const octokit = getOctokit(input.token);
 
-  const ownerRepo = {
-    owner: input.org,
-    repo: input.repo,
-  };
-
   let usage = {
     billable: {
       UBUNTU: {
@@ -48,55 +45,81 @@ export const run = async (): Promise<void> => {
       WINDOWS: {
         total_ms: 0,
       },
-      total_ms: 0
+      total_ms: 0,
+      included_minutes: 0,
+      total_paid_minutes_used: 0,
     },
   };
-  let workflowsIds: (string | number)[] = [];
-  if (input.workflows) {
-    workflowsIds = input.workflows.split(",").map((workflow) => workflow.trim());
-  } else {
-    const { data: workflows } = await octokit.rest.actions.listRepoWorkflows(ownerRepo);
-    workflowsIds = workflows.workflows.map((workflow) => workflow.id);
-  }
-  info(`Getting usage for ${workflowsIds.length} workflows (in minutes)...`);
-
-  for (const workflowsId of workflowsIds) {
-    try {
-      const { data } = await octokit.rest.actions.getWorkflowUsage({
-        ...ownerRepo,
-        workflow_id: workflowsId,
-      });
-
-      const _usage = [
-        data.billable?.UBUNTU?.total_ms,
-        data.billable?.MACOS?.total_ms,
-        data.billable?.WINDOWS?.total_ms
-      ].map((usage) => usage || 0)
-      const total = _usage.reduce((acc, curr) => acc + curr, 0);
-      usage.billable.UBUNTU.total_ms += _usage[0];
-      usage.billable.MACOS.total_ms += _usage[1];
-      usage.billable.WINDOWS.total_ms += _usage[2];
-      usage.billable.total_ms += total;
-      startGroup(`Workflow: ${workflowsId} - ${msToMinutes(total)}`);
-      info(`Ubuntu: ${msToMinutes(data.billable?.UBUNTU?.total_ms) || 0}`);
-      info(`MacOS: ${msToMinutes(data.billable?.MACOS?.total_ms) || 0}`);
-      info(`Windows: ${msToMinutes(data.billable?.WINDOWS?.total_ms) || 0}`);
-      endGroup();
-    } catch (err) {
-      info(`Error getting usage for workflows: ${workflowsId}`);
-      error(JSON.stringify(err));
+  if (input.repo) {
+    const ownerRepo = {
+      owner: input.org,
+      repo: input.repo,
+    };
+    let workflowsIds: (string | number)[] = [];
+    if (input.workflows) {
+      workflowsIds = input.workflows.split(",").map((workflow) => workflow.trim());
+    } else {
+      const { data: workflows } = await octokit.rest.actions.listRepoWorkflows(ownerRepo);
+      workflowsIds = workflows.workflows.map((workflow) => workflow.id);
     }
+    info(`Getting usage for ${workflowsIds.length} workflows (in minutes)...`);
+  
+    for (const workflowsId of workflowsIds) {
+      try {
+        const { data } = await octokit.rest.actions.getWorkflowUsage({
+          ...ownerRepo,
+          workflow_id: workflowsId,
+        });
+  
+        const _usage = [
+          data.billable?.UBUNTU?.total_ms,
+          data.billable?.MACOS?.total_ms,
+          data.billable?.WINDOWS?.total_ms
+        ].map((usage) => usage || 0)
+        const total = _usage.reduce((acc, curr) => acc + curr, 0);
+        usage.billable.UBUNTU.total_ms += _usage[0];
+        usage.billable.MACOS.total_ms += _usage[1];
+        usage.billable.WINDOWS.total_ms += _usage[2];
+        usage.billable.total_ms += total;
+        startGroup(`Workflow: ${workflowsId} - ${msToMinutes(total)}`);
+        info(`Ubuntu: ${msToMinutes(data.billable?.UBUNTU?.total_ms) || 0}`);
+        info(`MacOS: ${msToMinutes(data.billable?.MACOS?.total_ms) || 0}`);
+        info(`Windows: ${msToMinutes(data.billable?.WINDOWS?.total_ms) || 0}`);
+        endGroup();
+      } catch (err) {
+        info(`Error getting usage for workflows: ${workflowsId}`);
+        error(JSON.stringify(err));
+      }
+    }
+  } else {
+    const { data } = await octokit.rest.billing.getGithubActionsBillingOrg({
+      org: input.org,
+    });
+    usage.billable.total_ms = data.total_minutes_used;
+    usage.billable.UBUNTU.total_ms = data.minutes_used_breakdown.UBUNTU || 0;
+    usage.billable.WINDOWS.total_ms = data.minutes_used_breakdown.WINDOWS || 0;
+    usage.billable.MACOS.total_ms = data.minutes_used_breakdown.MACOS || 0;
+    usage.billable.total_paid_minutes_used = data.total_paid_minutes_used;
+    usage.billable.included_minutes = data.included_minutes;
   }
 
   info(`✅ Completed!`);
   info(`Total Ubuntu: ${msToMinutes(usage.billable.UBUNTU.total_ms)}`);
   info(`Total MacOS: ${msToMinutes(usage.billable.MACOS.total_ms)}`);
   info(`Total Windows: ${msToMinutes(usage.billable.WINDOWS.total_ms)}`);
+  if (usage.billable.included_minutes) {
+    info(`Included minutes: ${usage.billable.included_minutes}`);
+  }
+  if (usage.billable.total_paid_minutes_used) {
+    info(`Total paid minutes used: ${usage.billable.total_paid_minutes_used}`);
+  }
   info(`Total: ${msToMinutes(usage.billable.total_ms)}`);
   setOutput("ubuntu", msToMinutes(usage.billable.UBUNTU.total_ms));
   setOutput("macos", msToMinutes(usage.billable.MACOS.total_ms));
   setOutput("windows", msToMinutes(usage.billable.WINDOWS.total_ms));
   setOutput("total", msToMinutes(usage.billable.total_ms));
+  setOutput("included_minutes", usage.billable.included_minutes);
+  setOutput("total_paid_minutes_used", usage.billable.total_paid_minutes_used);
 };
 
 run();
